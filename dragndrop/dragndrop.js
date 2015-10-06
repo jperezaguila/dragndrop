7983c61c52c4a8593d7cec78b788e12feceeb20c
PRUEBA2 <jperezaguila@yaho.es>
2015-10-06 12:03:04 +0200

    drag and drop

---
+++ dragndrop/dragndrop.js
@@ -0,0 +1,38 @@
+﻿if (!sessionStorage.getItem("nombre"))location.replace("index.html");
+
+
+//1º registrar los eventos
+var idactual = undefined;
+
+function drag(evt) {
+//asocia al evento la informacion en este cado id
+    evt.dataTransfer.setData("objeto",evt.target.id);
+}
+
+function allowdrop(evt) {
+    evt.preventDefault();
+}
+
+function drop(evt) {
+    evt.preventDefault();
+    //var data = evt.dataTransfer.getData("objeto");
+
+    document.getElementById("destino")
+        .appendChild(document.getElementById(idactual));
+    idactual = undefined;
+
+    //evt.target.appendChild(document.getElementById(data));
+}
+//
+var image = document.querySelectorAll("img");
+var destino = document.querySelector("#destino");
+
+//
+for (var i = 0; i < image.length; i++) {
+    image[i].addEventListener("dragstart", drag);
+}
+
+destino.addEventListener("dragover", allowdrop);
+destino.addEventListener("drop", drop);
+
+
---
+++ dragndrop/iniciosesion.js
@@ -0,0 +1,13 @@
+﻿if (sessionStorage.getItem("nombre")) {
+    location.replace("index.html");
+}
+
+function login() {
+ if (document.getElementById("txtnom").value == "") {
+     alert("nombre vacio");
+     return;
+ }
+    sessionStorage.setItem("nombre", document.getElementById("txtnom").value);
+    location.replace("inicio.html");
+}
+document.getElementById("btnok").addEventListener("click", login);
